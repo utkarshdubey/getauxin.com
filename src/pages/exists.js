7c862bee9d74f3f89e588a890fff3ce6ba3f671e
Utkarsh Dubey <utkarsh.dubey3jan@gmail.com>
2020-03-23 17:42:45 +0530

    r

---
+++ src/pages/exists.js
@@ -0,0 +1,46 @@
+import React from "react"
+import { graphql, StaticQuery } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+import "../utils/normalize.css"
+import "../utils/css/screen.css"
+
+const ExistsPage = ({ data }, location) => {
+  const siteTitle = data.site.siteMetadata.title
+
+  return (
+    <Layout title={siteTitle}>
+      <SEO title="Exists" keywords={[`exists`]} />
+
+      <article className="post-content page-template no-image">
+        <div className="post-content-body">
+            <h2 id="clean-minimal-and-deeply-customisable-london-is-a-theme-made-for-people-who-appreciate-simple-lines-">
+                Exists
+            </h2>
+            <p style={{textAlign: 'center'}}>You have already been registered. If you think this is an error, reach out to us on <a href="mailto:getauxin@gmail.com">getauxin@gmail.com</a> or <a href="https://discord.gg/993sWfS">discord</a>.</p>
+        </div>
+      </article>
+    </Layout>
+  )
+}
+
+const indexQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    
+  }
+`
+export default props => (
+  <StaticQuery
+    query={indexQuery}
+    render={data => (
+      <ExistsPage location={props.location} data={data} {...props} />
+    )}
+  />
+)
